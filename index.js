cad5c46c0c4155c9c715111bbaf741730af5ba6b
spvky <spvky@users.noreply.github.com>
2025-07-31 14:25:26 +0000

    deploy: 989fed01599d36e48df52d2afc8678394537ae71

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpk4et_hxz.js
+// include: /tmp/tmp5e7nd09i.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -250,21 +250,21 @@ Module['FS_createPath']("/assets/asset_pack/items and trap_animation", "torch", 
 
   })();
 
-// end include: /tmp/tmpk4et_hxz.js
-// include: /tmp/tmpuwo_6i5d.js
+// end include: /tmp/tmp5e7nd09i.js
+// include: /tmp/tmpib37iiv7.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpuwo_6i5d.js
-// include: /tmp/tmpts6x2ctv.js
+  // end include: /tmp/tmpib37iiv7.js
+// include: /tmp/tmpcwah41tz.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpts6x2ctv.js
+  // end include: /tmp/tmpcwah41tz.js
 
 
 var arguments_ = [];
